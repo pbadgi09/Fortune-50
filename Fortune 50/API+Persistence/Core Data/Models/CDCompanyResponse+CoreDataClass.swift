8f8e0f52359f8e355b60fcae89de4e0e0b59bcba
pbadgi09 <pbadgi16@gmail.com>
2022-12-07 13:15:55 -0800

    Added Core Data Model

---
+++ Fortune 50/API+Persistence/Core Data/Models/CDCompanyResponse+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDCompanyResponse+CoreDataClass.swift
+//  Fortune 50
+//
+//  Created by Pranav Badgi on 12/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDCompanyResponse)
+public class CDCompanyResponse: NSManagedObject {
+
+}
---
+++ Fortune 50/API+Persistence/Core Data/Models/CDCompanyResponse+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  CDCompanyResponse+CoreDataProperties.swift
+//  Fortune 50
+//
+//  Created by Pranav Badgi on 12/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CDCompanyResponse {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDCompanyResponse> {
+        return NSFetchRequest<CDCompanyResponse>(entityName: "CDCompanyResponse")
+    }
+
+    @NSManaged public var fmt: String?
+    @NSManaged public var longFmt: String?
+    @NSManaged public var raw: Int32
+    @NSManaged public var name: String?
+    @NSManaged public var symbol: String?
+    @NSManaged public var isFavourited: Bool
+
+}
+
+extension CDCompanyResponse : Identifiable {
+
+}
