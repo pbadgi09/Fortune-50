06f67bef0c5f32e97d05b3311cad582264dd11ef
pbadgi09 <pbadgi16@gmail.com>
2022-12-07 12:53:36 -0800

    Detailed VC done

--- Fortune 50/Controllers/CompanyDetails/CompanyDetailsVC.swift
+++ Fortune 50/Controllers/CompanyDetails/CompanyDetailsVC.swift
@@ -11,6 +11,78 @@ final class CompanyDetailsVC: UIViewController {
     
     //MARK: - Properties
     
+    //  Symbol Label
+    private let symbolLabel: LabelView = {
+        let label                       = LabelView(withInset: 16, 16, 16, 16)
+        label.backgroundColor           = .label
+        label.titlePlaceholderColor     = .systemBackground
+        label.fontName                  = UIFont.boldSystemFont(ofSize: 25)
+        label.layer.cornerRadius        = 16
+        label.clipsToBounds             = true
+        return label
+    }()
+    
+    //  Market Cap Details
+    private let marketCapLabel: LabelView = {
+        let label               = LabelView(withInset: 0, 0, 0, 0)
+        label.titlePlaceholder  = "Market Cap"
+        label.alignment         = .center
+        label.fontName          = UIFont.boldSystemFont(ofSize: 25)
+        return label
+    }()
+    
+    private let fmtLabel: LabelView = {
+        let label                       = LabelView(withInset: 12, 12, 10, 10)
+        label.alignment                 = .center
+        label.backgroundColor           = .systemBlue.withAlphaComponent(0.5)
+        label.titlePlaceholderColor     = .label
+        label.layer.cornerRadius        = 16
+        label.clipsToBounds             = true
+        return label
+    }()
+    
+    private let longFmtLabel: LabelView = {
+        let label                       = LabelView(withInset: 12, 12, 10, 10)
+        label.alignment                 = .center
+        label.backgroundColor           = .systemGreen.withAlphaComponent(0.5)
+        label.titlePlaceholderColor     = .label
+        label.layer.cornerRadius        = 16
+        label.clipsToBounds             = true
+        return label
+    }()
+    
+    private let rawLabel: LabelView = {
+        let label                       = LabelView(withInset: 12, 12, 10, 10)
+        label.alignment                 = .center
+        label.backgroundColor           = .systemYellow.withAlphaComponent(0.5)
+        label.titlePlaceholderColor     = .label
+        label.layer.cornerRadius        = 16
+        label.clipsToBounds             = true
+        return label
+    }()
+    
+    private let marketCapStackView: UIStackView = {
+        let sv              = UIStackView()
+        sv.spacing          = 16
+        sv.axis             = .vertical
+        sv.distribution     = .fillEqually
+        sv.alignment        = .fill
+        return sv
+    }()
+    
+    
+    private let addToFavouriteButton: ButtonView = {
+        let button              = ButtonView()
+        button.font             = UIFont.boldSystemFont(ofSize: 18)
+        button.tintcolor        = .systemBackground
+        button.backgroundcolor  = .label
+        button.isLargeButton    = true
+        button.placeholder      = "Add to Favourites"
+        button.buttonImage      = UIImage(systemName: "heart")
+        return button
+    }()
+    
+    
     
     
     
@@ -121,13 +193,38 @@ final class CompanyDetailsVC: UIViewController {
     
     private func configureViews() {
         //  add sub views
-        
-        
+        view.addSubview(symbolLabel)
+        view.addSubview(marketCapStackView)
+        marketCapStackView.addArrangedSubview(marketCapLabel)
+        marketCapStackView.addArrangedSubview(fmtLabel)
+        marketCapStackView.addArrangedSubview(longFmtLabel)
+        marketCapStackView.addArrangedSubview(rawLabel)
+        view.addSubview(addToFavouriteButton)
         
         //  layout views
+        symbolLabel.centerX(view)
+        symbolLabel.constraint(top: navigationController?.navigationBar.bottomAnchor, paddingTop: 16)
         
+        //  market cap stack view
+        marketCapStackView.constraint(top: symbolLabel.bottomAnchor,
+                                      left: view.leftAnchor,
+                                      bottom: nil,
+                                      right: view.rightAnchor,
+                                      paddingTop: 32, paddingLeft: 16,
+                                      paddingBottom: 0, paddingRight: 16)
         
-        
+        //  favourite button
+        addToFavouriteButton.constraint(left: view.leftAnchor,
+                                        bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                        right: view.rightAnchor,
+                                        paddingLeft: 32,
+                                        paddingBottom: 16,
+                                        paddingRight: 32)
+        DispatchQueue.main.async { [weak self] in
+            guard let self                                  = self else { return }
+            let height                                      = self.addToFavouriteButton.frame.size.height
+            self.addToFavouriteButton.layer.cornerRadius    = height / 2
+        }
     }
     
     
@@ -143,7 +240,7 @@ final class CompanyDetailsVC: UIViewController {
     
     /// Adds recognizers and targets
     private func addRecognizers() {
-        
+        addToFavouriteButton.addTarget(self, action: #selector(addToFavouritesButtonTapped), for: .touchUpInside)
     }
     
     
@@ -154,10 +251,23 @@ final class CompanyDetailsVC: UIViewController {
     
     private func updateViews() {
         guard let companyResponse = companyResponse else { return }
-        title = companyResponse.name
+        title               = companyResponse.name
+        symbolLabel.text    = companyResponse.symbol
+        fmtLabel.setAttributedText("Fmt: ", companyResponse.marketCap.fmt, .label, .label, UIFont.systemFont(ofSize: 16), UIFont.boldSystemFont(ofSize: 18))
+        longFmtLabel.setAttributedText("Long Fmt: ", companyResponse.marketCap.longFmt, .label, .label, UIFont.systemFont(ofSize: 16), UIFont.boldSystemFont(ofSize: 18))
+        rawLabel.setAttributedText("Raw: ", "\(companyResponse.marketCap.raw)", .label, .label, UIFont.systemFont(ofSize: 16), UIFont.boldSystemFont(ofSize: 18))
     }
     
     
+    
+    
+    
+    
+    
+    
+    private func handleAddToFavourites() {
+        print("DEBUG: Handle Add to Fav Here")
+    }
     
     
     
@@ -174,7 +284,11 @@ final class CompanyDetailsVC: UIViewController {
     
     //MARK: - Selectors
     
-    
+    @objc private func addToFavouritesButtonTapped() {
+        addToFavouriteButton.bounce { [unowned self] in
+            handleAddToFavourites()
+        }
+    }
     
     
     
