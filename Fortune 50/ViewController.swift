20bc5c42f83106c506f47dc82e3fe06de92c152e
pbadgi09 <pbadgi16@gmail.com>
2022-12-07 10:20:06 -0800

    Initial Commit

---
+++ Fortune 50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fortune 50
+//
+//  Created by Pranav Badgi on 12/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
